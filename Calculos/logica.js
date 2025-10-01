1d36517778adc7a220c9c3ed9d4fdaf4a362b700
ismaelnata01-maker <ismaelnata01@gmail.com>
2025-10-01 17:06:09 -0300

    Add files via upload

---
+++ Calculos/logica.js
@@ -0,0 +1,20 @@
+function soma(){
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+
+    // console.log(numero1, numero2)
+
+    var resposta = document. getElementById("RespSoma")
+
+    resposta.textContent = numero1 + numero2
+}
+    function sub(){
+        var numero3 = document.getElementById("n3").valueAsNumber
+    var numero4 = document.getElementById("n4").valueAsNumber
+
+    // console.log(numero1, numero2)
+
+    var resposta = document. getElementById("RespSub")
+
+    resposta.textContent = numero3 - numero4
+    }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # forja-tarde-logica-01
 primeiro repositório lógica da turma
+
+#tags
+label - escreva do portugol
+h1 - escreva titulo do portugol
+input - leia do portugol br
+
+#git
+git clone "nome do repositorio"
